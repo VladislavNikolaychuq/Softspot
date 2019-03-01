773e7f41a3bbbc1ecbee04533599a87ed8e6ca22
VladislavNikolaychuk <shadou86@gmail.com>
2019-03-01 14:37:46 +0200

    Wpn GO

---
+++ wp-content/themes/SoftSpot/functions.php
@@ -0,0 +1,4 @@
+<?php
+define( 'TEMPLATEINC', TEMPLATEPATH . '/inc' );
+define( 'TEMPLATEURI', get_template_directory_uri() );
+define( 'DIRECT', TEMPLATEURI.'/assets/' );
---
+++ wp-content/themes/SoftSpot/single-pets.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vladislavnikolaychuck
+ * Date: 2019-03-01
+ * Time: 14:22
+ */
